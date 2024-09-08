875c3fd314678049d8eb5862ac67ac25280665ab
Wilson Liang <wilson.l20010327@gmail.com>
2024-09-07 23:19:18 -0400

    Create Course Init UnitTest

--- IndividualMiniprojectC++/test/CourseUnitTests.cpp
+++ IndividualMiniprojectC++/test/CourseUnitTests.cpp
@@ -3,21 +3,34 @@
 
 #include "Course.h"
 
-class CourseUnitTests : public ::testing::Test {
- protected:
-  static Course* testCourse;
+TEST(CourseUnitTests, EmptyInit) {
+  Course emptyCourse = Course();
+  // Expect equality.
+  EXPECT_EQ(emptyCourse.getCourseLocation(), "");
+  EXPECT_EQ(emptyCourse.getInstructorName(), "");
+  EXPECT_EQ(emptyCourse.getCourseTimeSlot(), "");
+  EXPECT_EQ(emptyCourse.isCourseFull(), false);
+}
+TEST(CourseUnitTests, DetialInit) {
+  Course detailCourse =
+      Course(250, "Griffin Newbold", "417 IAB", "11:40-12:55");
+  // Expect equality.
+  EXPECT_EQ(detailCourse.getCourseLocation(), "Griffin Newbold");
+  EXPECT_EQ(detailCourse.getInstructorName(), "417 IAB");
+  EXPECT_EQ(detailCourse.getCourseTimeSlot(), "11:40-12:55");
+  EXPECT_EQ(detailCourse.isCourseFull(), false);
+}
 
-  static void SetUpTestSuite() {
-    testCourse = new Course(250, "Griffin Newbold", "417 IAB", "11:40-12:55");
-  }
-
-  static void TearDownTestSuite() { delete testCourse; }
-};
-
-Course* CourseUnitTests::testCourse = nullptr;
-
-TEST_F(CourseUnitTests, ToStringTest) {
-  std::string expectedResult =
-      "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
-  ASSERT_EQ(expectedResult, testCourse->display());
+TEST(CourseUnitTests, EmptyInitDisplay) {
+  Course emptyCourse = Course();
+  // Expect equality.
+  EXPECT_EQ(emptyCourse.display(), "\nInstructor: ; Location: ; Time: ");
+}
+TEST(CourseUnitTests, DetialInitDisplay) {
+  Course detailCourse =
+      Course(250, "Griffin Newbold", "417 IAB", "11:40-12:55");
+  // Expect equality.
+  EXPECT_EQ(
+      detailCourse.display(),
+      "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55");
 }
